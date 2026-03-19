dbd41542c4e94c92a673a8282c9e9ab71aeb0b6c
abhinauya26-netizen <abhinauya26@gmail.com>
2026-03-19 13:52:14 +0530

    UC4 implemented

---
+++ src/main/java/HelloAp.java
@@ -0,0 +1,16 @@
+class HelloAp {
+
+    public static void main(String[] args) {
+
+        String message;
+
+        if (args.length > 0) {
+            String names = String.join(", ", args);
+            message = "Hello, " + names + "!";
+        } else {
+            message = "Hello, World!";
+        }
+
+        System.out.println(message);
+    }
+}
